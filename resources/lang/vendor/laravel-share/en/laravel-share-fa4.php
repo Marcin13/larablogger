88b98657568e534fe4703f00bb01a20b50ae9ac5
Marcin <whocallme.com>
2021-03-01 11:12:43 +0000

    social media css2

--- resources/lang/vendor/laravel-share/en/laravel-share-fa4.php
+++ resources/lang/vendor/laravel-share/en/laravel-share-fa4.php
@@ -2,10 +2,10 @@
 
 return [
     //'facebook' => '<li><a href=":url" class="social-button :class" id=":id" title=":title"><span class="fa fa-facebook-official"></span></a></li>',
-    'facebook' => '<li><a href=":url" class="social-button btn btn-sm btn-social btn-fb :class" id=":id" title="Share this post on Facebook :title"><span class="fa fa-facebook-square"></span>Share</a></li>',
+    'facebook' => '<li><a href=":url" class="social-button btn btn-sm btn-social btn-fb :class" id=":id" title="Share this post on Facebook :title"><span class="fa fa-facebook-square"></span></a></li>',
 
     //'twitter' => '<li><a href=":url" class="social-button :class" id=":id" title=":title"><span class="fa fa-twitter"></span></a></li>',
-    'twitter' => '<li><a href=":url" class="social-button btn btn-sm btn-social btn-tw :class" id=":id" title="Share this post on Twitter :title"><span class="fa fa-twitter"></span>Tweet</a></li>',
+    'twitter' => '<li><a href=":url" class="social-button btn btn-sm btn-social btn-tw :class" id=":id" title="Share this post on Twitter :title"><span class="fa fa-twitter"></span></a></li>',
 
     //'linkedin' => '<li><a href=":url" class="social-button :class" id=":id" title=":title"><span class="fa fa-linkedin"></span></a></li>',
     'linkedin' => '<li><a href=":url" class="social-button btn btn-sm btn-social btn-in :class" id=":id" title="Share this post on LinkedIn :title"><span class="fa fa-linkedin" data-fa-transform="grow-2"></span></a></li>',
